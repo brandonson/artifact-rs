c34e11d27fcfe112c51b1cd7c0331761977bf8ab
Brandon Sanderson <bas.bsanders@gmail.com>
2015-06-03 15:13:26 -0700

    Add a couple of tests.
    Use codifyle to check that file output works properly.
    We really need other tests as well, but things that go
    to stdout/stderr will probably be harder.

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,9 @@ extern crate lazy_static;
 #[cfg(feature = "time")]
 extern crate time;
 
+#[cfg(test)]
+extern crate codifyle;
+
 use std::thread::JoinHandle;
 
 pub use level::LogLevel;
@@ -59,4 +62,57 @@ impl ArtifactGlobalLib{
     }
   }
 
+}
+
+#[cfg(test)]
+mod file_test{
+  use std::path::PathBuf;
+  use super::{Logger, LoggerOutput, ArtifactGlobalLib, DEBUG, MessageFormatter, SimpleMessageFormatter};
+  use codifyle;
+
+  struct FooFormat;
+
+  impl MessageFormatter for FooFormat {
+    fn format_message(&self, logger:&str, level_string:&str, message: &str) -> String {
+      format!("Foobled from {} -- {}: {}", logger, level_string, message)
+    }
+
+    fn add_logger_name_to_multi_message(&self, _:&str, m:&str) -> String {
+      m.to_string()
+    }
+  }
+
+  #[test]
+  fn test_multi_file_write() {
+    let artifact_global = ArtifactGlobalLib::init();
+
+    let logger = Logger::new("Foo", LoggerOutput::FileLog(PathBuf::from("foolog.log")));
+    let logger_two = Logger::new_with_level("Bar", LoggerOutput::FileLog(PathBuf::from("foolog.log")), DEBUG);
+    logger.set_format(Box::new(FooFormat) as Box<MessageFormatter>);
+    logger.warning("This is a file log.");
+
+    logger_two.set_format(Box::new(SimpleMessageFormatter) as Box<MessageFormatter>);
+    logger_two.debug("And we can have two loggers to one file at once.");
+
+    artifact_global.stop();
+    codifyle::assert_file_contents_eq(
+      "foolog.log",
+      "Foobled from Foo -- WARNING: This is a file log.\
+      \n[Bar] -- DEBUG: And we can have two loggers to one file at once.\n");
+  }
+
+  #[test]
+  fn test_default_logger_set() {
+    let artifact_global = ArtifactGlobalLib::init();
+
+    let logger = Logger::new("DEFCHANGE", LoggerOutput::FileLog(PathBuf::from("foolog.log")));
+    Logger::set_default_formatter(Box::new(FooFormat) as Box<MessageFormatter>);
+    logger.warning("This will have the default format of FooFormat");
+
+    artifact_global.stop();
+
+    codifyle::assert_file_contents_eq(
+      "foolog.log",
+      "Foobled from DEFCHANGE -- WARNING: This will have the default format of FooFormat\n");
+  }
 }
